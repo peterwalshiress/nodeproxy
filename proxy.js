bef5114823eed9b85ebc9973c44f0d083f150cf0
peterwalshiress <31662438+peterwalshiress@users.noreply.github.com>
2017-09-05 16:51:33 +0100

    update

--- proxy.js
+++ proxy.js
@@ -4,23 +4,13 @@ var port = process.env.PORT || 8080;
 
 var addrRegex = /^(([a-zA-Z\-\.0-9]+):)?(\d+)$/;
 
-var addr = {
-    from: [port.toString()],
-    to: addrRegex.exec("http://52.169.89.213:9089")
-};
-
-console.log(addr);
-
-if (!addr.from || !addr.to) {
-    console.log('Usage: <from> <to>');
-}
 
 
 net.createServer(function(from) {
     var to = net.createConnection({
-        host: addr.to[2],
-        port: addr.to[3]
+        host: "52.169.89.213",
+        port: "9089"
     });
     from.pipe(to);
     to.pipe(from);
-}).listen(addr.from[0]);
+}).listen(port);
